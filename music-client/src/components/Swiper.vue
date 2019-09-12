1b662fd7e61ff9dcf15eb773fe2f2300f12905e1
Yin-Hongwei <3374461366@qq.com>
2019-09-12 13:01:06 +0800

    [修复]修复了已知问题

--- music-client/src/components/Swiper.vue
+++ music-client/src/components/Swiper.vue
@@ -34,6 +34,27 @@
 <script>
 export default {
   name: 'swiper'
+  // data () {
+  //   return {
+  //     swiperList: [{
+  //       picImg: '../assets/img/swiper/blur-1851426_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/concert-768722_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/boy-984293_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/ipad-605439_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/microphone-1209816_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/music-notes-3221097_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/piano-1655558_640.jpg'
+  //     }, {
+  //       picImg: '../assets/img/swiper/turntable-1337986_640.jpg'
+  //     }]
+  //   }
+  // }
 }
 </script>
 
