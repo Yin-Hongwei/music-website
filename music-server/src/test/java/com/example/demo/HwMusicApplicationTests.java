cf55be80a8a03c6f7b4e25a4ae73733fd672a8c0
Yin-Hongwei <3374461366@qq.com>
2019-12-14 18:45:14 +0800

    [修改]添加echarts依赖

--- music-server/src/test/java/com/example/demo/HwMusicApplicationTests.java
+++ music-server/src/test/java/com/example/demo/HwMusicApplicationTests.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.domain.*;
-import com.example.demo.service.impl.*;
+import com.example.demo.service.impl.ConsumerServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -96,19 +93,19 @@ public class HwMusicApplicationTests {
 @Test
 public void consumerTest(){
 
-    Consumer consumer = new Consumer();
-    consumer.setUsername("12333");
-    consumer.setPassword("123");
-    consumer.setSex(new Byte("1"));
-    consumer.setPhoneNum("13776412237");
-    consumer.setEmail("123@qq.com");
-    consumer.setBirth(new Date());
-    consumer.setIntroduction("");
-    consumer.setLocation("");
-    consumer.setAvator("/img/user.jpg");
-    consumer.setCreateTime(new Date());
-    consumer.setUpdateTime(new Date());
-    consumerService.ifAdd(consumer);
+//    Consumer consumer = new Consumer();
+//    consumer.setUsername("12333");
+//    consumer.setPassword("123");
+//    consumer.setSex(new Byte("1"));
+//    consumer.setPhoneNum("13776412237");
+//    consumer.setEmail("123@qq.com");
+//    consumer.setBirth(new Date());
+//    consumer.setIntroduction("");
+//    consumer.setLocation("");
+//    consumer.setAvator("/img/user.jpg");
+//    consumer.setCreateTime(new Date());
+//    consumer.setUpdateTime(new Date());
+//    consumerService.ifAdd(consumer);
 }
 //    @Test
 //    public void consumerTest2()
