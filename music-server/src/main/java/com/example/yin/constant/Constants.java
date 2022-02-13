f9201b76b885e11950c10e337a8214483b38be23
綦枫 <1255187803@qq.com>
2022-02-14 03:36:02 +0800

    1、windows系统自动读取项目路径，去除代码中的地址硬编码。

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     // MAC、Linux系统
     public static final String RESOURCE_MAC_PATH = "/Users/hongweiyin/Documents/github-workspace/music-website/music-server";
 
-    // windos系统
-    public static final String RESOURCE_WIN_PATH = "C:\\Users\\hongweiyin\\Documents\\github-workspace\\music-website\\music-server";
+    // windows系统
+    public static final String RESOURCE_WIN_PATH = System.getProperty("user.dir");
 }
