e6cd4f6df10f34a209132781799cc839fdaad3db
綦枫 <1255187803@qq.com>
2022-02-15 04:44:28 +0800

    綦枫 Commit： 1、无变更

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -1,6 +1,8 @@
 package com.example.yin.constant;
 
 public class Constants {
+
+    /* 歌曲图片，歌手图片，歌曲文件，歌单图片等文件的存放路径 */
     public static String PROJECT_PATH = System.getProperty("user.dir");
     public static String AVATOR_IMAGES_PATH = "file:" + PROJECT_PATH + "/music-server/img/avatorImages/";
     public static String SONGLIST_PIC_PATH = "file:" + PROJECT_PATH + "/music-server/img/songListPic/";
