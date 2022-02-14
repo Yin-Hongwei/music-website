2df509fcdf917c6f58d663ac8725a1b46d2ca087
綦枫 <1255187803@qq.com>
2022-02-15 04:46:52 +0800

    綦枫 Commit： 1、无变更

--- music-client/src/mixins/index.js
+++ music-client/src/mixins/index.js
@@ -1,19 +1,5 @@
-import {mapGetters} from 'vuex'
-import {
-  HOME,
-  SONG_SHEET,
-  SINGER,
-  MY_MUSIC,
-  SETTING,
-  SIGN_IN,
-  SIGN_UP,
-  SIGN_OUT,
-  SEARCH,
-  LYRIC,
-  SONG_SHEET_DETAIL,
-  SINGER_DETAIL,
-  ERROR
-} from '../enums'
+import { mapGetters } from 'vuex'
+import { HOME, SONG_SHEET, SINGER, MY_MUSIC, SETTING, SIGN_IN, SIGN_UP, SIGN_OUT, SEARCH, LYRIC, SONG_SHEET_DETAIL, SINGER_DETAIL, ERROR } from '../enums'
 
 const mixin = {
   computed: {
@@ -26,9 +12,7 @@ const mixin = {
   methods: {
     // 获取图片信息
     attachImageUrl (srcUrl) {
-      if (!srcUrl) {
-        return this.BASE_URL + srcUrl
-      } else return '../assets/images/user.jpg'
+      return this.BASE_URL + srcUrl || '../assets/images/user.jpg'
     },
     getBirth (val) {
       let birth = String(val).match(/[0-9-]+(?=\s)/)
@@ -53,53 +37,53 @@ const mixin = {
       this.$store.commit('setActiveNavName', value)
     },
     // 播放
-    playMusic ({id, url, pic, index, name, lyric, currentSongList}) {
+    playMusic ({ id, url, pic, index, name, lyric, currentSongList }) {
       const songTitle = this.getSongTitle(name)
       const singerName = this.getSingerName(name)
       this.$store.dispatch('playMusic', {id, url, pic, index, songTitle, singerName, lyric, currentSongList})
     },
     // 路由管理
-    routerManager (routerName, {path, query}) {
+    routerManager (routerName, { path, query }) {
       switch (routerName) {
         case HOME:
         case SIGN_OUT:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SONG_SHEET:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SONG_SHEET_DETAIL:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SINGER:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SINGER_DETAIL:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case MY_MUSIC:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SETTING:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SIGN_IN:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SIGN_UP:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case SEARCH:
-          this.$router.push({path, query})
+          this.$router.push({ path, query })
           break
         case LYRIC:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         case ERROR:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
         default:
-          this.$router.push({path})
+          this.$router.push({ path })
           break
       }
     },
