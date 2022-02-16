3dab410610d2799a6511320a6c3bd335307c59d2
Yin-Hongwei <yinhongwei96@126.com>
2022-02-17 01:29:46 +0800

    fix: 解决mac系统资源加载失败的问题

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -2,12 +2,14 @@ package com.example.yin.constant;
 
 public class Constants {
 
+    public static String ASSETS_PATH = System.getProperty("os.name").toLowerCase().startsWith("win") ? "/music-server" : "";
+
     /* 歌曲图片，歌手图片，歌曲文件，歌单图片等文件的存放路径 */
-    public static String PROJECT_PATH = System.getProperty("user.dir");
-    public static String AVATOR_IMAGES_PATH = "file:" + PROJECT_PATH + "/music-server/img/avatorImages/";
-    public static String SONGLIST_PIC_PATH = "file:" + PROJECT_PATH + "/music-server/img/songListPic/";
-    public static String SONG_PIC_PATH = "file:" + PROJECT_PATH + "/music-server/img/songPic/";
-    public static String SONG_PATH = "file:" + PROJECT_PATH + "/music-server/song/";
-    public static String SINGER_PIC_PATH = "file:" + PROJECT_PATH + "/music-server/img/singerPic/";
+    public static String PROJECT_PATH = System.getProperty("user.dir") + ASSETS_PATH;
+    public static String AVATOR_IMAGES_PATH = "file:" + PROJECT_PATH + "/img/avatorImages/";
+    public static String SONGLIST_PIC_PATH = "file:" + PROJECT_PATH + "/img/songListPic/";
+    public static String SONG_PIC_PATH = "file:" + PROJECT_PATH + "/img/songPic/";
+    public static String SONG_PATH = "file:" + PROJECT_PATH + "/song/";
+    public static String SINGER_PIC_PATH = "file:" + PROJECT_PATH + "/img/singerPic/";
 
 }
