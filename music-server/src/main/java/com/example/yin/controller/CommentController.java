fa73b89f04c75bea1ecf91fa26cd4a44d6f49d4d
Winston.Li <zqdely@163.com>
2022-03-13 16:34:34 +0800

    删除并且格式化了Controller

--- music-server/src/main/java/com/example/yin/controller/CommentController.java
+++ music-server/src/main/java/com/example/yin/controller/CommentController.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.example.yin.domain.Comment;
 import com.example.yin.service.impl.CommentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,16 +17,16 @@ public class CommentController {
     @Autowired
     private CommentServiceImpl commentService;
 
-//  提交评论
+    //  提交评论
     @ResponseBody
     @RequestMapping(value = "/comment/add", method = RequestMethod.POST)
-    public Object addComment(HttpServletRequest req){
+    public Object addComment(HttpServletRequest req) {
 
         JSONObject jsonObject = new JSONObject();
         String user_id = req.getParameter("userId");
         String type = req.getParameter("type");
-        String song_list_id=req.getParameter("songListId");
-        String song_id=req.getParameter("songId");
+        String song_list_id = req.getParameter("songListId");
+        String song_id = req.getParameter("songId");
         String content = req.getParameter("content").trim();
 
         Comment comment = new Comment();
@@ -41,72 +40,72 @@ public class CommentController {
         comment.setContent(content);
         comment.setCreateTime(new Date());
         boolean res = commentService.addComment(comment);
-        if (res){
+        if (res) {
             jsonObject.put("code", 1);
             jsonObject.put("msg", "评论成功");
             return jsonObject;
-        }else {
+        } else {
             jsonObject.put("code", 0);
             jsonObject.put("msg", "评论失败");
             return jsonObject;
         }
     }
 
-//    获取所有评论列表
+    //    获取所有评论列表
     @RequestMapping(value = "/comment", method = RequestMethod.GET)
-    public Object allComment(){
+    public Object allComment() {
         return commentService.allComment();
     }
 
-//    获得指定歌曲ID的评论列表
+    //    获得指定歌曲ID的评论列表
     @RequestMapping(value = "/comment/song/detail", method = RequestMethod.GET)
-    public Object commentOfSongId(HttpServletRequest req){
+    public Object commentOfSongId(HttpServletRequest req) {
         String songId = req.getParameter("songId");
         return commentService.commentOfSongId(Integer.parseInt(songId));
     }
 
-//    获得指定歌单ID的评论列表
+    //    获得指定歌单ID的评论列表
     @RequestMapping(value = "/comment/songList/detail", method = RequestMethod.GET)
-    public Object commentOfSongListId(HttpServletRequest req){
+    public Object commentOfSongListId(HttpServletRequest req) {
         String songListId = req.getParameter("songListId");
         return commentService.commentOfSongListId(Integer.parseInt(songListId));
     }
 
-//    点赞
+    //    点赞
     @ResponseBody
     @RequestMapping(value = "/comment/like", method = RequestMethod.POST)
-    public Object commentOfLike(HttpServletRequest req){
+    public Object commentOfLike(HttpServletRequest req) {
 
-    JSONObject jsonObject = new JSONObject();
-    String id = req.getParameter("id").trim();
-    String up = req.getParameter("up").trim();
+        JSONObject jsonObject = new JSONObject();
+        String id = req.getParameter("id").trim();
+        String up = req.getParameter("up").trim();
 
-    Comment comment = new Comment();
-    comment.setId(Integer.parseInt(id));
-    comment.setUp(Integer.parseInt(up));
-    boolean res = commentService.updateCommentMsg(comment);
-    if (res){
-        jsonObject.put("code", 1);
-        jsonObject.put("msg", "点赞成功");
-        return jsonObject;
-    }else {
-        jsonObject.put("code", 0);
-        jsonObject.put("msg", "点赞失败");
-        return jsonObject;
+        Comment comment = new Comment();
+        comment.setId(Integer.parseInt(id));
+        comment.setUp(Integer.parseInt(up));
+        boolean res = commentService.updateCommentMsg(comment);
+        if (res) {
+            jsonObject.put("code", 1);
+            jsonObject.put("msg", "点赞成功");
+            return jsonObject;
+        } else {
+            jsonObject.put("code", 0);
+            jsonObject.put("msg", "点赞失败");
+            return jsonObject;
+        }
     }
-}
 
-//    删除评论
+    //    删除评论
     @RequestMapping(value = "/comment/delete", method = RequestMethod.GET)
-    public Object deleteComment(HttpServletRequest req){
+    public Object deleteComment(HttpServletRequest req) {
         String id = req.getParameter("id");
         return commentService.deleteComment(Integer.parseInt(id));
     }
 
-//    更新评论
+    //    更新评论
     @ResponseBody
     @RequestMapping(value = "/comment/update", method = RequestMethod.POST)
-    public Object updateCommentMsg(HttpServletRequest req){
+    public Object updateCommentMsg(HttpServletRequest req) {
         JSONObject jsonObject = new JSONObject();
         String id = req.getParameter("id").trim();
         String user_id = req.getParameter("userId").trim();
@@ -135,11 +134,11 @@ public class CommentController {
         comment.setUp(Integer.parseInt(up));
 
         boolean res = commentService.updateCommentMsg(comment);
-        if (res){
+        if (res) {
             jsonObject.put("code", 1);
             jsonObject.put("msg", "修改成功");
             return jsonObject;
-        }else {
+        } else {
             jsonObject.put("code", 0);
             jsonObject.put("msg", "修改失败");
             return jsonObject;
--- music-server/src/main/java/com/example/yin/controller/ListSongController.java
+++ music-server/src/main/java/com/example/yin/controller/ListSongController.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.example.yin.domain.ListSong;
 import com.example.yin.service.impl.ListSongServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,53 +17,53 @@ public class ListSongController {
     @Autowired
     private ListSongServiceImpl listSongService;
 
-//    给歌单添加歌曲
+    //    给歌单添加歌曲
     @ResponseBody
     @RequestMapping(value = "/listSong/add", method = RequestMethod.POST)
-    public Object addListSong(HttpServletRequest req){
-    JSONObject jsonObject = new JSONObject();
-    String song_id = req.getParameter("songId").trim();
-    String song_list_id = req.getParameter("songListId").trim();
+    public Object addListSong(HttpServletRequest req) {
+        JSONObject jsonObject = new JSONObject();
+        String song_id = req.getParameter("songId").trim();
+        String song_list_id = req.getParameter("songListId").trim();
 
-    ListSong listsong = new ListSong();
-    listsong.setSongId(Integer.parseInt(song_id));
-    listsong.setSongListId(Integer.parseInt(song_list_id));
+        ListSong listsong = new ListSong();
+        listsong.setSongId(Integer.parseInt(song_id));
+        listsong.setSongListId(Integer.parseInt(song_list_id));
 
-    boolean res = listSongService.addListSong(listsong);
-    if (res){
-        jsonObject.put("code", 1);
-        jsonObject.put("msg", "添加成功");
-    }else {
-        jsonObject.put("code", 0);
-        jsonObject.put("msg", "添加失败");
-    }
+        boolean res = listSongService.addListSong(listsong);
+        if (res) {
+            jsonObject.put("code", 1);
+            jsonObject.put("msg", "添加成功");
+        } else {
+            jsonObject.put("code", 0);
+            jsonObject.put("msg", "添加失败");
+        }
         return jsonObject;
     }
 
-//    返回歌单里包含的所有歌曲
+    //    返回歌单里包含的所有歌曲
     @RequestMapping(value = "/listSong", method = RequestMethod.GET)
-    public Object allListSong(){
+    public Object allListSong() {
         return listSongService.allListSong();
     }
 
-//    返回歌单里指定歌单ID的歌曲
+    //    返回歌单里指定歌单ID的歌曲
     @RequestMapping(value = "/listSong/detail", method = RequestMethod.GET)
-    public Object listSongOfSongId(HttpServletRequest req){
+    public Object listSongOfSongId(HttpServletRequest req) {
         String songListId = req.getParameter("songListId");
         return listSongService.listSongOfSongId(Integer.parseInt(songListId));
     }
 
-//    删除歌单里的歌曲
+    //    删除歌单里的歌曲
     @RequestMapping(value = "/listSong/delete", method = RequestMethod.GET)
-    public Object deleteListSong(HttpServletRequest req){
+    public Object deleteListSong(HttpServletRequest req) {
         String songId = req.getParameter("songId");
         return listSongService.deleteListSong(Integer.parseInt(songId));
     }
 
-//    更新歌单里面的歌曲信息
+    //    更新歌单里面的歌曲信息
     @ResponseBody
     @RequestMapping(value = "/listSong/update", method = RequestMethod.POST)
-    public Object updateListSongMsg(HttpServletRequest req){
+    public Object updateListSongMsg(HttpServletRequest req) {
         JSONObject jsonObject = new JSONObject();
         String id = req.getParameter("id").trim();
         String song_id = req.getParameter("songId").trim();
@@ -76,11 +75,11 @@ public class ListSongController {
         listsong.setSongListId(Integer.parseInt(song_list_id));
 
         boolean res = listSongService.updateListSongMsg(listsong);
-        if (res){
+        if (res) {
             jsonObject.put("code", 1);
             jsonObject.put("msg", "修改成功");
             return jsonObject;
-        }else {
+        } else {
             jsonObject.put("code", 0);
             jsonObject.put("msg", "修改失败");
             return jsonObject;
