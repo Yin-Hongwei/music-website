217ee55a0ede10779c7760ee29c72fdb1424f991
Winston.Li <zqdely@163.com>
2022-03-13 16:34:34 +0800

    删除并且格式化了Controller

--- music-server/src/main/java/com/example/yin/controller/SongListController.java
+++ music-server/src/main/java/com/example/yin/controller/SongListController.java
@@ -1,12 +1,11 @@
 package com.example.yin.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.yin.constant.Constants;
 import com.example.yin.domain.SongList;
 import com.example.yin.service.impl.SongListServiceImpl;
-import com.example.yin.constant.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
-@CrossOrigin
 @RestController
 public class SongListController {
 
