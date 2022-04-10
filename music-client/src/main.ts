1dcdc6734d3e89e4bea677742055f6a1b1aea974
Yin-Hongwei <yinhongwei96@126.com>
2022-04-11 03:51:37 +0800

    style: 修改代码格式

--- music-client/src/main.ts
+++ music-client/src/main.ts
@@ -10,12 +10,10 @@ import "./assets/icons/index.js";
 import { ComponentCustomProperties } from "vue";
 import { Store } from "vuex";
 declare module "@vue/runtime-core" {
-  // 声明自己的 store state
   interface State {
     count: number;
   }
 
-  // 为 `this.$store` 提供类型声明
   interface ComponentCustomProperties {
     $store: Store<State>;
   }
