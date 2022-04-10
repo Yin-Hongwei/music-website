46a9eeaceb8d5ff2e18689a101c52a49327a999e
Yin-Hongwei <yinhongwei96@126.com>
2022-04-11 03:51:37 +0800

    style: 修改代码格式

--- music-client/src/views/YinContainer.vue
+++ music-client/src/views/YinContainer.vue
@@ -23,7 +23,6 @@ if (sessionStorage.getItem("dataStore")) {
   proxy.$store.replaceState(Object.assign( {}, proxy.$store.state, JSON.parse(sessionStorage.getItem("dataStore"))));
 }
 
-// 页面刷新时将 vuex 里的信息保存到 sessionStorage
 window.addEventListener("beforeunload", () => {
   sessionStorage.setItem("dataStore", JSON.stringify(proxy.$store.state));
 });
