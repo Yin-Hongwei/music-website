436c1fc07ce13276d4796f3481864ba9c2c43eaa
Yin-Hongwei <yinhongwei96@126.com>
2022-04-21 21:38:26 +0800

    fix: 修复收藏歌曲的bug

--- music-client/src/mixins/mixin.ts
+++ music-client/src/mixins/mixin.ts
@@ -2,6 +2,7 @@ import { getCurrentInstance, computed } from "vue";
 import { useStore } from "vuex";
 import { LocationQueryRaw } from "vue-router";
 import { RouterName } from "@/enums";
+import { HttpManager } from "@/api";
 
 interface routerOptions {
   path?: string;
@@ -32,12 +33,14 @@ export default function () {
     return str.split("-")[0];
   }
 
-  function checkStatus() {
+  // 判断登录状态
+  function checkStatus(status?: boolean) {
     if (!token.value) {
-      (proxy as any).$message({
-        message: "请先登录",
-        type: "warning",
-      });
+      if (status !== false)
+        (proxy as any).$message({
+          message: "请先登录",
+          type: "warning",
+        });
       return false;
     }
     return true;
@@ -59,6 +62,30 @@ export default function () {
     });
   }
 
+  // 下载
+  async function downloadMusic({ songUrl, songName }) {
+    if (!songUrl) {
+      (proxy as any).$message({
+        message: "下载链接为空！",
+        type: "error",
+      });
+      console.error("下载链接为空！");
+      return;
+    }
+
+    const result = (await HttpManager.downloadMusic(songUrl)) as ResponseBody;
+    const eleLink = document.createElement("a");
+    eleLink.download = `${songName}.mp3`;
+    eleLink.style.display = "none";
+    // 字符内容转变成 blob 地址
+    const blob = new Blob([result.data]);
+    eleLink.href = URL.createObjectURL(blob);
+    document.body.appendChild(eleLink); // 触发点击
+    eleLink.click();
+    document.body.removeChild(eleLink); // 移除
+  }
+
+  // 导航索引
   function changeIndex(value) {
     proxy.$store.commit("setActiveNavName", value);
   }
@@ -100,5 +127,6 @@ export default function () {
     playMusic,
     routerManager,
     goBack,
+    downloadMusic,
   };
 }
--- music-server/src/main/java/com/example/yin/controller/CollectController.java
+++ music-server/src/main/java/com/example/yin/controller/CollectController.java
@@ -2,11 +2,9 @@ package com.example.yin.controller;
 
 import com.example.yin.common.ErrorMessage;
 import com.example.yin.common.SuccessMessage;
-import com.example.yin.common.WarningMessage;
 import com.example.yin.domain.Collect;
 import com.example.yin.service.impl.CollectServiceImpl;
 
-import org.apache.commons.lang3.ObjectUtils.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,10 +31,6 @@ public class CollectController {
         String song_id = req.getParameter("songId");
         String song_list_id = req.getParameter("songListId");
 
-        if (collectService.existSongId(Integer.parseInt(user_id), Integer.parseInt(song_id))) {
-            return new WarningMessage("已收藏").getMessage();
-        }
-
         Collect collect = new Collect();
         collect.setUserId(Integer.parseInt(user_id));
         collect.setType(new Byte(type));
@@ -49,23 +43,37 @@ public class CollectController {
 
         boolean res = collectService.addCollection(collect);
         if (res) {
-            return new SuccessMessage<Null>("收藏成功").getMessage();
+            return new SuccessMessage<Boolean>("收藏成功", true).getMessage();
         } else {
             return new ErrorMessage("收藏失败").getMessage();
         }
     }
 
     // 取消收藏的歌曲
-    @RequestMapping(value = "/collection/delete", method = RequestMethod.GET)
+    @RequestMapping(value = "/collection/delete", method = RequestMethod.POST)
     public Object deleteCollection(HttpServletRequest req) {
         String user_id = req.getParameter("userId").trim();
         String song_id = req.getParameter("songId").trim();
 
         boolean res = collectService.deleteCollect(Integer.parseInt(user_id), Integer.parseInt(song_id));
         if (res) {
-            return new SuccessMessage<Null>("取消收藏").getMessage();
+            return new SuccessMessage<Boolean>("取消收藏", false).getMessage();
         } else {
             return new ErrorMessage("取消收藏失败").getMessage();
+        }
+    }
+
+    // 是否收藏歌曲
+    @RequestMapping(value = "/collection/status", method = RequestMethod.POST)
+    public Object isCollection(HttpServletRequest req) {
+        String user_id = req.getParameter("userId").trim();
+        String song_id = req.getParameter("songId").trim();
+
+        boolean res = collectService.existSongId(Integer.parseInt(user_id), Integer.parseInt(song_id));
+        if (res) {
+            return new SuccessMessage<Boolean>("已收藏", true).getMessage();
+        } else {
+            return new SuccessMessage<Boolean>("未收藏", false).getMessage();
         }
     }
 
