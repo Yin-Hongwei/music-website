daab22117b3cc7d08806416b00862e24bcf467a1
asus <836585692@qq.com>
2022-06-10 16:41:26 +0800

    6.10 沉住气 认真修订

--- music-server/src/main/java/com/example/yin/mapper/ListSongMapper.java
+++ music-server/src/main/java/com/example/yin/mapper/ListSongMapper.java
@@ -4,25 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.yin.model.domain.ListSong;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ListSongMapper extends BaseMapper<ListSong> {
 
-
-    int insertSelective(ListSong record);
-
-    ListSong selectByPrimaryKey(Integer id);
-
-
-
-    int updateByPrimaryKey(ListSong record);
-
-    int updateListSongMsg(ListSong record);
-
-    int deleteListSong(Integer songId);
-
-    List<ListSong> allListSong();
-
-    List<ListSong> listSongOfSongId(Integer songListId);
 }
