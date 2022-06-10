738d55c611c0ce842df23ea8fe0093c376826c11
asus <836585692@qq.com>
2022-06-10 16:48:14 +0800

    6.10 沉住气 认真修订

--- music-server/src/main/java/com/example/yin/mapper/SingerMapper.java
+++ music-server/src/main/java/com/example/yin/mapper/SingerMapper.java
@@ -4,28 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.yin.model.domain.Singer;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SingerMapper extends BaseMapper<Singer> {
 
-    int insert(Singer record);
-
-    int insertSelective(Singer record);
-
-    Singer selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKey(Singer record);
-
-    int updateSingerMsg(Singer record);
-
-    int updateSingerPic(Singer record);
-
-    int deleteSinger(Integer id);
-
-    List<Singer> allSinger();
-
-    List<Singer> singerOfName(String name);
-
-    List<Singer> singerOfSex(Integer sex);
 }
