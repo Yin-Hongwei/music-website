885827e27501d74ce1157a99e5c638ca7ec62ec2
asus <836585692@qq.com>
2022-06-11 22:08:26 +0800

    6.11 解决用户可以重复点赞bug 新增表

--- music-server/src/main/java/com/example/yin/service/impl/UserSupportServiceImpl.java
+++ music-server/src/main/java/com/example/yin/service/impl/UserSupportServiceImpl.java
@@ -12,13 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
-* @author asus
-* @description 针对表【user_support】的数据库操作Service实现
-* @createDate 2022-06-11 16:06:28
-*/
+ * @author asus
+ * @description 针对表【user_support】的数据库操作Service实现
+ * @createDate 2022-06-11 16:06:28
+ */
 @Service
 public class UserSupportServiceImpl extends ServiceImpl<UserSupportMapper, UserSupport>
-implements UserSupportService{
+        implements UserSupportService {
 
     @Autowired
     private UserSupportMapper userSupportMapper;
@@ -26,20 +26,20 @@ implements UserSupportService{
     @Override
     public R isUserSupportComment(UserSupportRequest userSupportRequest) {
         QueryWrapper<UserSupport> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("comment_id",userSupportRequest.getCommentId());
-        queryWrapper.eq("user_id",userSupportRequest.getUserId());
-        if (userSupportMapper.selectCount(queryWrapper)>0){
-            return R.success("您已取消点赞",true);
-        }else {
-            return R.success("您已点赞",false);
+        queryWrapper.eq("comment_id", userSupportRequest.getCommentId());
+        queryWrapper.eq("user_id", userSupportRequest.getUserId());
+        if (userSupportMapper.selectCount(queryWrapper) > 0) {
+            return R.success("您已取消点赞", true);
+        } else {
+            return R.success("您已点赞", false);
         }
     }
 
     @Override
     public R insertCommentSupport(UserSupportRequest userSupportRequest) {
         UserSupport userSupport = new UserSupport();
-        BeanUtils.copyProperties(userSupportRequest,userSupport);
-        if (userSupportMapper.insert(userSupport)>0) {
+        BeanUtils.copyProperties(userSupportRequest, userSupport);
+        if (userSupportMapper.insert(userSupport) > 0) {
             return R.success("添加记录成功");
         }
         return R.error("添加时,发生异常");
@@ -48,9 +48,9 @@ implements UserSupportService{
     @Override
     public R deleteCommentSupport(UserSupportRequest userSupportRequest) {
         QueryWrapper<UserSupport> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("comment_id",userSupportRequest.getCommentId());
-        queryWrapper.eq("user_id",userSupportRequest.getUserId());
-        if (userSupportMapper.delete(queryWrapper)>0){
+        queryWrapper.eq("comment_id", userSupportRequest.getCommentId());
+        queryWrapper.eq("user_id", userSupportRequest.getUserId());
+        if (userSupportMapper.delete(queryWrapper) > 0) {
             return R.success("删除记录成功");
         }
         return R.error("删除记录发生异常");
