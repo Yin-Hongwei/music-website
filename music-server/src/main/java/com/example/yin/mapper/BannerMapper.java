0e3b8601b4cfd7cc418850e8f626982cda7c843b
asus <836585692@qq.com>
2022-06-13 15:50:23 +0800

    6.13 轮播图存入redis  直接从非关系型数据库中读取 不需要每次都去数据库中查   虽然这个项目轮播图是从前端拿的

---
+++ music-server/src/main/java/com/example/yin/mapper/BannerMapper.java
@@ -0,0 +1,15 @@
+package com.example.yin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.yin.model.domain.Banner;
+
+/**
+* @author asus
+* @description 针对表【banner】的数据库操作Mapper
+* @createDate 2022-06-13 13:13:42
+* @Entity generator.domain.Banner
+*/
+public interface BannerMapper extends BaseMapper<Banner> {
+
+
+}
