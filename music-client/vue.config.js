479e009b99f1ca4e828300ced0631c52d12539d1
Yin-Hongwei <yinhongwei96@126.com>
2022-09-24 00:11:01 +0800

    fix: 解决轮播图首次显示不全的bug

--- music-client/vue.config.js
+++ music-client/vue.config.js
@@ -3,11 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
-        Object.assign(definitions[0]['process.env'], {
-          // NODE_HOST: '"http://localhost:8888"',
-            NODE_HOST: '"https://music-backend.onlyicanstopmyself.top"',
-        });
-        return definitions;
+      Object.assign(definitions[0]['process.env'], {
+        NODE_HOST: '"http://localhost:8888"',
+      });
+      return definitions;
     });
   }
 })
