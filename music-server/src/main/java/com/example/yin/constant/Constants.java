215e7c267e115a4ca0fbeac06dd582e8600a9f63
Yin-Hongwei <yinhongwei96@126.com>
2023-04-25 23:04:36 +0800

    fix: 修复win资源不显示问题
    fix: 修复资源不显示问题
    
    fix: 修复删除歌手的bug
    
    fix: 修复资源不显示问题

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -2,14 +2,14 @@ package com.example.yin.constant;
 
 public class Constants {
     /* 歌曲图片，歌手图片，歌曲文件，歌单图片等文件的存放路径 */
-    public static String PROJECT_PATH = System.getProperty("user.dir");
-    public static String AVATOR_IMAGES_PATH = "file:" + PROJECT_PATH + "/img/avatorImages/";
-    public static String SONGLIST_PIC_PATH = "file:" + PROJECT_PATH + "/img/songListPic/";
-    public static String SONG_PIC_PATH = "file:" + PROJECT_PATH + "/img/songPic/";
-    public static String SONG_PATH = "file:" + PROJECT_PATH + "/song/";
-    public static String SINGER_PIC_PATH = "file:" + PROJECT_PATH + "/img/singerPic/";
-    public static String BANNER_PIC_PATH = "file:" + PROJECT_PATH + "/img/swiper/";
-
+    public static String ASSETS_PATH = System.getProperty("user.dir").replaceAll("\\", "/");
+    
+    public static String AVATOR_IMAGES_PATH = "file:" + ASSETS_PATH + "/img/avatorImages/";
+    public static String SONGLIST_PIC_PATH = "file:" + ASSETS_PATH + "/img/songListPic/";
+    public static String SONG_PIC_PATH = "file:" + ASSETS_PATH + "/img/songPic/";
+    public static String SONG_PATH = "file:" + ASSETS_PATH + "/song/";
+    public static String SINGER_PIC_PATH = "file:" + ASSETS_PATH + "/img/singerPic/";
+    public static String BANNER_PIC_PATH = "file:" + ASSETS_PATH + "/img/swiper/";
 
     /* 盐值加密 */
     public static String SALT = "zyt";
