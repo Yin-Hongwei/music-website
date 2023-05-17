dc2523975cc97e0cbf99e2f3c0aec0064f42155e
Yin-Hongwei <yinhongwei96@126.com>
2023-05-18 00:59:31 +0800

    fix: 修复资源不显示问题

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -2,15 +2,14 @@ package com.example.yin.constant;
 
 public class Constants {
     /* 歌曲图片，歌手图片，歌曲文件，歌单图片等文件的存放路径 */
-    public static String DIR_PATH = System.getProperty("user.dir");
-    public static String ASSETS_PATH = DIR_PATH + (System.getProperty("os.name").toLowerCase().startsWith("win") ? "/music-server" : "");
+    public static String ASSETS_PATH = System.getProperty("user.dir").replaceAll("\\", "/");
+    
     public static String AVATOR_IMAGES_PATH = "file:" + ASSETS_PATH + "/img/avatorImages/";
     public static String SONGLIST_PIC_PATH = "file:" + ASSETS_PATH + "/img/songListPic/";
     public static String SONG_PIC_PATH = "file:" + ASSETS_PATH + "/img/songPic/";
     public static String SONG_PATH = "file:" + ASSETS_PATH + "/song/";
     public static String SINGER_PIC_PATH = "file:" + ASSETS_PATH + "/img/singerPic/";
     public static String BANNER_PIC_PATH = "file:" + ASSETS_PATH + "/img/swiper/";
-
 
     /* 盐值加密 */
     public static String SALT = "zyt";
