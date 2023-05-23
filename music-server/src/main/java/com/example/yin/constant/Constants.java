25c3e494dcc95f3740539ea6e844b2e38a47c995
Yin-Hongwei <yinhongwei96@126.com>
2023-05-23 09:50:02 +0800

    fix: 修复启动问题

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -2,7 +2,7 @@ package com.example.yin.constant;
 
 public class Constants {
     /* 歌曲图片，歌手图片，歌曲文件，歌单图片等文件的存放路径 */
-    public static String ASSETS_PATH = System.getProperty("user.dir").replaceAll("\\", "/");
+    public static String ASSETS_PATH = System.getProperty("user.dir");
     
     public static String AVATOR_IMAGES_PATH = "file:" + ASSETS_PATH + "/img/avatorImages/";
     public static String SONGLIST_PIC_PATH = "file:" + ASSETS_PATH + "/img/songListPic/";
