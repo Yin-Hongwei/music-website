0b32318b83c3f82b9f29d92b1dc0598841669a4f
liunan3 <liunan3@asiainfo.com>
2024-10-29 12:51:30 +0800

    歌手->艺人

--- music-client/src/components/layouts/YinPlayBar.vue
+++ music-client/src/components/layouts/YinPlayBar.vue
@@ -166,7 +166,7 @@ export default defineComponent({
       "songId", // 音乐id
       "songUrl", // 音乐地址
       "songTitle", // 歌名
-      "singerName", // 歌手名
+      "singerName", // 艺人名
       "songPic", // 歌曲图片
       "curTime", // 当前音乐的播放位置
       "duration", // 音乐时长
